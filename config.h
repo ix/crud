7f9e9c665b9ff577b0f44265a63ee221299eca4e
Rose <rose@empty.town>
2020-08-14 11:11:15 +0100

    add comments for the config flags

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
-#define FOREGROUND_COLOR 0x888888
-#define BORDER 4
-#define PERSISTENT_GRABBING 0
+#define FOREGROUND_COLOR 0x888888 // color to use for the selection border, RGB
+#define BORDER 4                  // width of the selection border, in pixels
+#define PERSISTENT_GRABBING 0     // if nonzero, crud will retry grabbing the cursor repeatedly if it initially fails
